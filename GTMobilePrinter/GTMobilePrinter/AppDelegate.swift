d92c1b4bcdd6d205bb0c53fc0ab1aaaf1af6de27
Yiqi (Victor) Chen <yiqic1993@gatech.edu>
2014-09-20 16:29:58 -0400

    check status function

--- GTMobilePrinter/GTMobilePrinter/AppDelegate.swift
+++ GTMobilePrinter/GTMobilePrinter/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        var mycontroller = self.window?.rootViewController
+        (mycontroller as ViewController).displayUsrAlert()
         return true
     }
     
