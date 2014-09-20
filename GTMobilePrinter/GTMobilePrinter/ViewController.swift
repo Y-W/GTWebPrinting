847019f06c178b8e7c69e7793b1657df590c5c4e
Yiqi (Victor) Chen <yiqic1993@gatech.edu>
2014-09-20 15:05:58 -0400

    send network request

--- GTMobilePrinter/GTMobilePrinter/ViewController.swift
+++ GTMobilePrinter/GTMobilePrinter/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIAlertViewDelegate {
+class ViewController: UIViewController, UIAlertViewDelegate, NSURLConnectionDataDelegate {
                             
     let SELECT_PRINTER_ALERT = 1
     let PUT_USR_ALERT = 2
     
     let serverhost = "192.168.1.120"
+    let path = "/printfile.php"
     var file: NSURL?
     
     override func viewDidLoad() {
@@ -26,7 +27,7 @@ class ViewController: UIViewController, UIAlertViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func selectPrinter(file: NSURL) {
+    func displayPrintAlert(file: NSURL) {
         var alertView = UIAlertView(title: "Print Document",
             message: "Please enter gt username", delegate: self,
             cancelButtonTitle: "Cancel",
@@ -38,14 +39,44 @@ class ViewController: UIViewController, UIAlertViewDelegate {
     }
     
     func printDocument(usr: String, printer: String) {
-        println(file!.absoluteString)
-        println(usr)
-        println(printer)
+        
+        var fileData = NSData(contentsOfURL: file)
+        var parameters = NSString(format: "ptr=%@&usr=%@", printer, usr)
+        var requestBody = NSMutableData()
+        requestBody.appendData(parameters.dataUsingEncoding(NSASCIIStringEncoding))
+//        requestBody.appendData(fileData)
+        
+        println(fileData.length)
+//        println(NSString(data: fileData, encoding: NSUTF8StringEncoding))
+        
+        var url = "http://".stringByAppendingString(serverhost)
+            .stringByAppendingString(path)
+//            .stringByAppendingString("?ptr=").stringByAppendingString(printer)
+//            .stringByAppendingString("&usr=").stringByAppendingString(usr)
+        
+        var postRequest = NSMutableURLRequest(URL: NSURL(string: url))
+//        postRequest.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+//        postRequest.setValue(requestBody.length, forKey: "Content-Length")
+//        postRequest.setValue(printer, forKey: "ptr")
+//        postRequest.setValue(usr, forKey: "usr")
+        postRequest.HTTPMethod = "POST"
+        postRequest.HTTPBody = requestBody
+        
+        var conn = NSURLConnection.connectionWithRequest(postRequest, delegate: self)
+        if conn != nil {
+            println("Request Successful")
+        }
+        else {
+            println("Request Failed")
+        }
+        
+//        println(fileData.length)
+//        println(usr)
+//        println(printer)
     }
     
     func alertView(alertView: UIAlertView!, didDismissWithButtonIndex buttonIndex: Int) {
         
-        println("hello world")
         if alertView.tag == SELECT_PRINTER_ALERT {
             var usr = alertView.textFieldAtIndex(0).text
             if buttonIndex == 1 {
@@ -55,6 +86,10 @@ class ViewController: UIViewController, UIAlertViewDelegate {
                 printDocument(usr, printer: "Mobile_color")
             }
         }
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        println(NSString(data: data, encoding: NSUTF8StringEncoding))
     }
 
 }
