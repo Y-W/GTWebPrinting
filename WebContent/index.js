a4d414b9ec91d0698e20eda039be207ad293219e
Yijie Wang <yijiewang@gatech.edu>
2014-09-20 15:34:24 -0400

    YW

--- WebContent/index.js
+++ WebContent/index.js
@@ -16,14 +16,14 @@ $(document).ready(function(){
 		formData.append("ptr", $('#ptr').val());
 		$.each(files, function(key, value)
 		{
-					formData.append(key, value);
+					formData.append("file", value);
 		});
 		console.log(formData);
 		
 		
 		//$.post("http://192.168.1.120/printfile.php", formData, function(data){});
 		$.ajax({
-	        url: 'http://192.168.1.120/printfile.php',
+	        url: 'printfile.php',
 	        type: 'POST',
 	        data: formData,
 	        cache: false,
